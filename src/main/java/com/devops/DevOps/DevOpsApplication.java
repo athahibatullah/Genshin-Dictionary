45df0efc58f2ae578cdb7c6c5ebcfe1f90450f4a
Hibatulllah, Atha Layana <atha.l.hibatulllah@accenture.com>
2022-10-12 16:35:28 +0700

    jenkins 2

--- src/main/java/com/devops/DevOps/DevOpsApplication.java
+++ src/main/java/com/devops/DevOps/DevOpsApplication.java
@@ -4,9 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DevOpsApplication extends SpringBootServletInitializer {
@@ -20,14 +17,14 @@ public class DevOpsApplication extends SpringBootServletInitializer {
 		return builder.sources(DevOpsApplication.class);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
-			}
-		};
-	}
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+//			}
+//		};
+//	}
 
 }
